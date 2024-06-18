d72eb243e06de1697491c1b528e0aa542659e4d3
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-06-18 21:37:20 +0700

    FEAT: Completed the skeleton of the activity plan page

--- src/app/activity-plan/page.tsx
+++ src/app/activity-plan/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@nextui-org/button'
 import React from 'react'
 import { IoIosAdd } from 'react-icons/io'
 import { format } from 'date-fns'
+import { Card, CardBody } from '@nextui-org/react'
 
 const months = [
     { value: 1, label: 'January' },
@@ -33,7 +34,7 @@ const page = () => {
     const thisDay = today(getLocalTimeZone())
     const todayMonth = months.find(month => month.value === thisDay.month)?.label
 
-    const getDayName = (date: CalendarDate) => { 
+    const getDayName = (date: CalendarDate) => {
         const dateObj = date.toDate(getLocalTimeZone())
         return format(dateObj, 'EEE')
     }
@@ -53,25 +54,37 @@ const page = () => {
             weekDates.push(nextDate)
         }
         return weekDates
-    }  
-    
+    }
+
     const weekDates = getWeekDates(thisDay)
 
     return (
         <div className='flex flex-col m-10'>
-            <div className='flex flex-row mb-8'>
+            <div className='flex flex-row mb-8 ml-5'>
                 <h3 className='font-custom text-3xl font-bold mr-4'>
                     {todayMonth}
                 </h3>
                 <Button className='self-start' radius='full' color='primary' variant='flat' endContent={<IoIosAdd />}> Add Session </Button>
             </div>
-            <div className='sm:grid md:grid-cols-7 sm:grid-cols-1'>
+            <div className='flex flex-wrap ml-5'>
                 {weekDates.map(day => (
-                    <div key={day.toString()} className="relative flex justify-between font-sans font-bold text-xl mr-10 pb-2">
-                        <span className={`self-end ${today(getLocalTimeZone()).toString() === day.toString() ? 'text-primary font-bold' : 'text-white font-medium opacity-30'}`}> {day.day}/{day.month} </span>
-                        <span className={`text-white text-2xl self-end font-extrabold ${today(getLocalTimeZone()).toString() === day.toString() ? 'opactiy-100' : 'opacity-30'}`}> {getDayName(day)} </span>
-                        <span className={`block w-full h-1 bg-white rounded-full absolute bottom-0 ${today(getLocalTimeZone()).toString() === day.toString() ? 'opactiy-100' : 'opacity-15'}`}></span>
-                    </div>
+                    <>
+                        <div className='flex flex-row w-full md:max-w-48 mr-6' key={day.toString()}>
+                            <div className='flex flex-col w-full mb-8 md:max-w-48'>
+                                <div className="relative flex justify-between font-sans font-bold text-xl pb-2">
+                                    <span className={`text-white text-2xl self-end font-extrabold ${today(getLocalTimeZone()).toString() === day.toString() ? 'opactiy-100' : 'opacity-30'}`}> {getDayName(day)} </span>
+                                    <span className={`self-end ${today(getLocalTimeZone()).toString() === day.toString() ? 'text-primary font-bold' : 'text-white font-medium opacity-30'}`}> {day.day}/{day.month} </span>
+                                    <span className={`block w-full h-1 bg-white rounded-full absolute bottom-0 ${today(getLocalTimeZone()).toString() === day.toString() ? 'opactiy-100' : 'opacity-15'}`}></span>
+                                </div>
+                                <Card className='mt-3 text-sm w-full mr-8'>
+                                    <CardBody>
+                                        <p> 9.00 - 11.00 </p>
+                                        <p> <span>Test: </span>RPL</p>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        </div>
+                    </>
                 ))}
             </div>
         </div>
